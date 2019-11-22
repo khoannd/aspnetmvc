1fb297b2734ee43cc3055972b23428c08c240164
Khoan Nguyen <khoannd@gmail.com>
2019-11-22 12:14:35 +0700

    First commit for the news project

---
+++ NewsManagement/NewsManagement/Models/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NewsManagement.Models
+{
+    public class UserCustom
+    {
+        [Required(ErrorMessage ="User name is required")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public bool Active { get; set; }
+    }
+}
